fe9e84635187c270d695a91700c9a638505f2009
Dev Kumar <devanshshukla957@gmail.com>
2026-09-14 23:17:18 +0530

    updates

---
+++ control_flow.py
@@ -0,0 +1,15 @@
+#Control Flow is an essential concept in computer programming that allows developers to dictate the order in which instructions are executed in a program
+
+#A program showing the control flow of in a program using conditions 
+marks = int(input("Enter the marks of a student:"))
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+elif marks >=  60:
+    print("Grade D")
+else:
+    print("Grade F")
+
