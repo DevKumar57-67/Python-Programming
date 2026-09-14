9a53882684a61f5149d1bf754058e4560340d638
Dev Kumar <devanshshukla957@gmail.com>
2026-09-14 23:24:13 +0530

    updates

---
+++ arrays.py
@@ -0,0 +1,12 @@
+#Array is a linear data structure that stores elements of the same type in a contiguous block of memory.
+#It stores a fixed-size sequential collection of elements of the same type 
+
+#Arrays in Python
+arr = [1,2,3,4,5]
+print(arr)
+print(arr*2)
+
+#Taking array as input 
+arr= list(map(int,input("Enter the elements of the array:").split()))
+print("The array is ", arr)
+
