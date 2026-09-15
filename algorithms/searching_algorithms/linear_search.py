61cd98c47a8165dddc7580a434236f1c333e3262
Dev Kumar <devanshshukla957@gmail.com>
2026-09-16 03:22:42 +0530

    merge_two_linked_lists.py

---
+++ algorithms/searching_algorithms/linear_search.py
@@ -0,0 +1,21 @@
+#Linear Search is a classic searching algorithm
+
+
+# Algorithm: 
+# Step1: traverse the whole array
+# Step2: than check if arr[i] == key element
+# Step3: the return the index
+
+def linear_search(arr,n,key):
+    for i in range(n):
+        if arr[i] == key:
+            return i
+    return -1
+
+#Calling the function
+arr = list(map(int, input("Enter the elements of the array: ").split()))
+n = len(arr)
+print("The size of the arrat is: ",n)
+key = int(input("Enter the key element:"))
+result = linear_search(arr,n,key)
+print("The key element is at index: ", result)
