f40efc68e95d7e328f7c28ddb219712d163bc965
Dev Kumar <devanshshukla957@gmail.com>
2026-09-18 20:07:15 +0530

    leetcode_problem

--- functions.py
+++ functions.py
@@ -6,7 +6,43 @@ def sum(a,b):
     return a+b
 
 #Calling the function
+
+
+#A Basic Calculator using python functions
+
+
+def diff(a,b):
+    return a-b
+
+
+def prod(a,b):
+    return a*b
+
+
+def div(a,b):
+    return a/b
+
+def mod(a,b):
+    return a%b
+
+def floatdiv(a,b):
+    return a//b
+
+
+#Calling the Function
+
 a= int(input("Enter a:"))
 b= int(input("Enter b:"))
-result = sum(a,b)
-print("The sum is:", result)
+add = sum(a,b)
+minus  = diff(a,b)
+mul = prod(a,b)
+divide = div(a,b)
+modulo = mod(a,b)
+floatdivi= floatdiv(a,b)
+
+print("The sum is:", add)
+print("The diff is:", minus)
+print("The prod is:", mul)
+print("The division is:", divide)
+print("The modulo is:", modulo)
+print("The floatdiv is:", floatdivi)
